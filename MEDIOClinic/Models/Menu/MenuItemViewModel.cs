b16e78e1d5631f49063f4c9b16307b07e236c515
giriraj13 <giriraj@dotstark.com>
2022-08-18 16:25:14 +0530

    first test

---
+++ MEDIOClinic/Models/Menu/MenuItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MEDIOClinic.Models
+{
+    public class MenuItemViewModel
+    {
+        public string MenuItemText { get; set; }
+        public string MenuItemRelativeUrl { get; set; }
+    }
+}
---
+++ MEDIOClinic/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,42 @@
+﻿using Kentico.Content.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using CMS.DocumentEngine;
+using CMS.Core;
+using CMS.DocumentEngine.Routing;
+using MEDIOClinic.Models;
+
+namespace MEDIOClinic.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly IPageRetriever pageRetriever;
+        private readonly IPageUrlRetriever pageUrlRetriever;
+
+        public NavigationMenuViewComponent(IPageRetriever pageRetriever, IPageUrlRetriever pageUrlRetriever)
+        {
+            this.pageRetriever = pageRetriever;
+            this.pageUrlRetriever = pageUrlRetriever;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<TreeNode> menuItems = await pageRetriever.RetrieveAsync<TreeNode>(query => query
+                                                    .MenuItems()
+                                                    .NestingLevel(1)
+                                                    .Columns("DocumentName", "NodeID", "NodeSiteID")
+                                                    .WithPageUrlPaths()
+                                                    .OrderByAscending("NodeOrder"));
+            IEnumerable<MenuItemViewModel> model = menuItems.Select(item => new MenuItemViewModel()
+            {
+                MenuItemText=item.DocumentName,
+                MenuItemRelativeUrl=pageUrlRetriever.Retrieve(item).RelativePath
+            });
+            return View(model);
+        }
+
+        
+    }
+}
